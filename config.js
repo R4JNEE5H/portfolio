e09768a5ba32d5b45602ff0e2a74d802dff77216
Rajneesh Thakur <rajneeshthakur26@gmail.com>
2026-03-01 17:39:31 +0530

    removed congig.js from gitignore

---
+++ config.js
@@ -0,0 +1,2 @@
+// Contact form endpoint — gitignored, not committed to source control
+window.FLOW_URL = 'https://691b26943eb9e7d486a36b0d62af88.d2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/aba25a9bc9424d21b4112bc9ba31203e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=0oqQN4ie3Qdipb0QYgjdPyYW_QxgyxMCJD00kQA7MDY';
