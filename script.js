751b651181dd5a1d696544e12a72d2fc8eed8a51
Rajneesh Thakur <rajneeshthakur26@gmail.com>
2026-03-01 16:55:24 +0530

    move flow url to gitignored config.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ canvas.addEventListener('mousemove', e => {
 
 /* ===== CONTACT FORM ===== */
 
-const FLOW_URL = 'https://691b26943eb9e7d486a36b0d62af88.d2.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/aba25a9bc9424d21b4112bc9ba31203e/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=0oqQN4ie3Qdipb0QYgjdPyYW_QxgyxMCJD00kQA7MDY'
+const FLOW_URL = window.FLOW_URL;
 function handleFormSubmit(e) {
   e.preventDefault();
   const btn = document.getElementById('submitBtn');
